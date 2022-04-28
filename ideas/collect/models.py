9b53d74bd84304f6de3c19f9545f1721e2dc57d8
dimka77783 <dimka77783@mai.ru>
2022-04-28 21:35:54 +0300

    8

---
+++ ideas/collect/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Collect(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    offer = models.TextField(blank=True)
+    result = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True)
