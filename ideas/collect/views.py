29a88256ba2093ba85862b983936bff9371e332f
dimka77783 <dimka77783@mai.ru>
2022-05-04 21:34:51 +0300

    11

--- ideas/collect/views.py
+++ ideas/collect/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Collect
 
 def index(request):
-    #  print(dir(request))
-    return HttpResponse('Hello world')
+    collect = Collect.objects.order_by('-created_at')
+    context = {
+        'collect': collect,
+        'title': 'Список идей'
+    }
+    return render(request, template_name='collect/index.html', context=context)
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
 
 
 
