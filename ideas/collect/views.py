e1ac17e65c317fb502d80da1f1f9864601b7015c
dimka77783 <dimka77783@mai.ru>
2022-05-22 20:10:12 +0300

    17

--- ideas/collect/views.py
+++ ideas/collect/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Collect
+from .models import Collect, Category
 
 def index(request):
     collect = Collect.objects.all()
+    categories = Category.objects.all()
     context = {
         'collect': collect,
-        'title': 'Список идей'
+        'title': 'Список идей',
+        'categories': categories
     }
-    return render(request, template_name='collect/index.html', context=context)
+    return render(request, 'collect/index.html', context)
 
-
+def get_category(request, category_id):
+    collect = Collect.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'collect/category.html', { 'collect':collect, 'categories':categories ,'category': category})
 
 
