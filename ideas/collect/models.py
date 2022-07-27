7b0126a7e17cd544299b94d5cef1617830992614
dimka77783 <dimka77783@mai.ru>
2022-07-27 20:35:57 +0300

    37

--- ideas/collect/models.py
+++ ideas/collect/models.py
@@ -11,6 +11,7 @@ class Collect(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Наименование категории эффекта')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_collect', kwargs={"pk": self.pk})
--- ideas/collect/templatetags/collect_tags.py
+++ ideas/collect/templatetags/collect_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.db.models import Count
 from collect.models import Category
 
 register = template.Library()
@@ -10,5 +10,6 @@ def get_categories():
 
 @register.inclusion_tag('collect/list_categories.html')
 def show_categories(arg1='Hello', arg2='world'):
-    categories = Category.objects.all()
+    #categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('collect')).filter(cnt__gt=0)
     return {"categories": categories, "arg1": arg1, "arg2": arg2}
