33d6a833d6cfe56a8b8fe9f6fd0c7c07a7c6d9f1
dimka77783 <dimka77783@mai.ru>
2022-08-14 21:39:04 +0300

    47

--- ideas/ideas/urls.py
+++ ideas/ideas/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('collect.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
